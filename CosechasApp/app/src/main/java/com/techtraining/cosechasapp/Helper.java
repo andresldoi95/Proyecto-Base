33f2eb191702d160865dd99c07ac987b01158671
Andres Leon Doylet <andres.leon@kimerasoft-ec.com>
2020-08-23 15:22:00 -0500

    Prueba en produccion

--- CosechasApp/app/src/main/java/com/techtraining/cosechasapp/Helper.java
+++ CosechasApp/app/src/main/java/com/techtraining/cosechasapp/Helper.java
@@ -1,11 +1,16 @@
 package com.techtraining.cosechasapp;
 
 public class Helper {
-    public static final String URL_API = "http://10.0.2.2:8000/api";
-    public static final String URL_OAUTH2 = "http://10.0.2.2:8000/oauth/token";
+    //public static final String URL_API = "http://10.0.2.2:8000/api";
+    //public static final String URL_OAUTH2 = "http://10.0.2.2:8000/oauth/token";
+    //http://38.123.149.8
+    public static final String URL_API = "http://38.123.149.8/api";
+    public static final String URL_OAUTH2 = "http://38.123.149.8/oauth/token";
     public static final String CLIENT_ID = "2";
     public static final String GRANT_TYPE = "password";
-    public static final String CLIENT_SECRET = "oFlPysSiwkL5wOp2T2ruCVHjd1zX4T4Ja8wPN6db";
+    //public static final String CLIENT_SECRET = "oFlPysSiwkL5wOp2T2ruCVHjd1zX4T4Ja8wPN6db";
+    //XbEO8Ol48yp67UQrw0L4sbQSrmNKxFGaRwzplHKL
+    public static final String CLIENT_SECRET = "XbEO8Ol48yp67UQrw0L4sbQSrmNKxFGaRwzplHKL";
     public static final String USER_TOKEN_NAME = "userToken";
     public static final String FIRST_TIME_NAME = "firstTime";
     public static final int MAX_IMAGE_SIZE = 20;
