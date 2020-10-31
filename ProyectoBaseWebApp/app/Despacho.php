24f9a5f83f34e617c18aae67a7dbc1e5d63ca3c6
Andres Leon Doylet <andres.leon@kimerasoft-ec.com>
2020-10-31 13:58:48 -0500

    Correccion de modelo

--- ProyectoBaseWebApp/app/Despacho.php
+++ ProyectoBaseWebApp/app/Despacho.php
@@ -10,7 +10,7 @@ class Despacho extends Model
         'id', 'camion_id', 'controlador_id', 'destino_id', 'aserrador_id', 'material_id', 'tipo_madera_id',
         'origen_madera_id', 'formato_entrega_id', 'codigo_po', 'fecha_tumba', 'fecha_despacho',
         'dias_t2k', 'guia_remision', 'guia_forestal', 'guia_forestal', 'tipo_llenado',
-        'valor_flete', 'estado', 'usuario_id', 'origen_hacienda_id'
+        'valor_flete', 'estado', 'usuario_id', 'origen_hacienda_id', 'numero_documento'
     ];
     public $incrementing = false;
     protected $dates = [
